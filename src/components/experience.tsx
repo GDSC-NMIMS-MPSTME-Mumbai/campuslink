1b19e1c58fc5c470e9b33c6352574f65cf057444
Akshaj05 <122291038+Akshaj05@users.noreply.github.com>
2024-03-23 07:31:47 +0530

    made the exp page dynamic

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Org from './org'
+import React from 'react';
+import Org from './org';
 
 const Experience = () => {
-return (
-    <div className="w-[87vw] bg-[#FFFF] mx-auto rounded-3xl font-mainFont border-[2px] border-[#E0EEEE] lg:w-[51vw]">
-        <div className="px-6 py-5 lg:px-8 lg:py-7">
-            <h1 className="font-500 text-2xl text-[#101010] lg:text-3xl">Experience</h1>
-            <Org />
-            <div className="border-t-2 border-dashed border-[#C0C0C066] mt-4"></div>
-            <Org />
+    return (
+        <div className="w-[87vw] bg-[#FFFF] mx-auto rounded-3xl font-mainFont border-[2px] border-[#E0EEEE] lg:w-[51vw]">
+            <div className="px-6 py-5 lg:px-8 lg:py-7">
+                <h1 className="font-500 text-2xl text-[#101010] lg:text-3xl">Experience</h1>
+                    <Org orgName="Meta" date="August, 2022 • Present" />
+                <div className="border-t-2 border-dashed border-[#C0C0C066] mt-4"></div>
+                    <Org orgName="Google" date="August, 2022 • Present" />
+            </div>
         </div>
-    </div>
-)
+    );
 }
 
-export default Experience
+export default Experience;
--- src/components/position.tsx
+++ src/components/position.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const Position = () => {
+interface OrgProps {
+  pos: string;
+  description: string;
+}
+
+const Position: React.FC<OrgProps> = ({pos,description}) => {
   return (
     <>
     <div className="flex gap-5">
@@ -8,9 +13,9 @@ const Position = () => {
       <div className="flex flex-col">
         <div className="flex gap-3">
           <img src="images/Arrow.svg" alt="" className='h-5 md:hidden '/>
-          <h1 className="text-[#101010] italic text-md lg:text-lg font-[400]">Sub-Head • 1 yr 3 mos</h1>
+          <h1 className="text-[#101010] italic text-md lg:text-lg font-[400]">{pos}</h1>
         </div>
-        <p className="text-[#606060] text-[0.89rem] lg:text-[0.9rem] text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, iure repellat. Vel autem praesentium id, nobis quidem mollitia laborum perspiciatis, incidunt reiciendis odit accusantium laboriosam quos esse. Fugiat, eligendi rerum!</p>
+        <p className="text-[#606060] text-[0.89rem] lg:text-[0.9rem] text-justify">{description}</p>
       </div>
     </div>
     </>
