b1d1f6215676fd598c662303a162568be8216806
Akshaj05 <122291038+Akshaj05@users.noreply.github.com>
2024-03-23 19:38:31 +0530

    arrow of exp fixed

--- src/components/position.tsx
+++ src/components/position.tsx
@@ -12,7 +12,7 @@ const Position: React.FC<OrgProps> = ({pos,description}) => {
       <img src="images/Exp_arrow.svg" alt="Arrow" className="h-12 md:w-16 hidden md:block" />
       <div className="flex flex-col">
         <div className="flex gap-3">
-          <img src="images/Arrow.svg" alt="" className='h-4 md:hidden '/>
+          <img src="images/Exp_arrow.svg" alt="" className='h-4 md:hidden '/>
           <h1 className="text-[#101010] italic text-sm lg:text-lg font-[400]">{pos}</h1>
         </div>
         <p className="text-[#606060] text-[0.8rem] leading-relaxed tracking-wide  lg:text-[0.9rem] text-justify">{description}</p>
